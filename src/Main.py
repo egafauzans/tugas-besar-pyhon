e49014ac7fe57255535bfe5ae6872cd02ec1542d
Ega Fauzan <egafauzan24@gmail.com>
2023-12-12 11:34:27 +0700

    tahap pre-processing data

--- src/Main.py
+++ src/Main.py
@@ -1,1 +1,16 @@
 ## tugas besar - pengolahan dan visualisasi data
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Membaca data dari file CSV pake pandas
+tes = pd.read_csv('tes.csv')
+
+## Pre-processing data
+# Bikin data frame 
+dataFrame = pd.DataFrame(tes)
+
+# data cleaning, pembersihan data
+dataFrame.isnull().sum() # ngecek ada data yang ilang ngga
+
